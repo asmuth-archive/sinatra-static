274c2480dd121f067422f4773bc127e741f9fa59
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2012-11-25 15:30:23 -0500

    Add failing tests

---
+++ test/build_test.rb
@@ -0,0 +1,32 @@
+require File.expand_path('../test_helper', __FILE__)
+
+class SinatraStaticBuildTest < UnitTest
+  include Rack::Test::Methods
+
+  class App < UnitTest::App
+    get '/' do
+      "homepage"
+    end
+    get '/contact' do
+      "contact"
+    end
+    get '/data.json' do
+      "{test: 'ok'}"
+    end
+  end
+
+  def test_build
+    # Temporary public folder
+    public_path = File.join(App.root, 'public')
+    FileUtils.rm_rf File.join(App.root, 'public')
+    FileUtils.mkdir public_path
+
+    builder = SinatraStatic.new(App)
+    builder.build!(public_path)
+
+    assert File.read(File.join(App.root, 'public/index.html')).include?('homepage')
+    assert File.read(File.join(App.root, 'public/contact/index.html')).include?('contact')
+    assert File.read(File.join(App.root, 'public/data.json')).include?("{test: 'ok'}")
+  end
+
+end
