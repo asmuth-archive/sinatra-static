854b522afef06e7283ed434edf49233e802cff80
Iain Barnett <iainspeed@gmail.com>
2015-12-09 18:32:55 +0000

    So it doesn't get caught by simplecov.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,7 +9,7 @@ Spec_dir = File.expand_path( File.dirname __FILE__ )
 # code coverage
 require 'simplecov'
 SimpleCov.start do
-  add_filter "/vendor/"
+  add_filter "/vendor.noindex/"
   add_filter "/bin/"
   add_filter "/spec/"
 end
